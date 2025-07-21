0d41edf56c265140ad8c2489363ff185b1f8516c
Bhabin Dulal <bhabindulal35@gmail.com>
2025-07-21 22:46:31 +0545

    basic project set up

---
+++ frontend/LmsApp/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Borrow Read Learn!!!<br />
+             Transform Your Knowledge</h1>
+
+        </div>
+    )
+}
+
+export default HomePage
