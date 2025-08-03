f5fbd5cbb44b1cd60208792e20ffe66266cb2cde
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-03 20:36:00 +0545

    authentication for admin

--- backend/authorization/auth.middleware.js
+++ backend/authorization/auth.middleware.js
@@ -1,14 +1,12 @@
 import jwt from 'jsonwebtoken';
 import User from "../models/user.models.js";
 
-
+// admin only acess!
 export const protect = async (req, res, next)=>{
     const authHeader = req.headers.authorization;
 
     if(authHeader && authHeader.startsWith("Bearer")){
         const token = authHeader.split(" ")[1];
-
-
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select("-password");
@@ -21,3 +19,4 @@ export const protect = async (req, res, next)=>{
     return res.status(401).json({ message: "No token provided" });
   }
 }
+
