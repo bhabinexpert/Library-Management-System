75d3c014fd5de6693fdc4f6cb598e4bfdc79bb1b
Bhabin Dulal <bhabindulal35@gmail.com>
2025-09-18 21:37:11 +0545

    Add mobile device detection and redirect to MobileNotice page; register new route

--- frontend/LmsApp/src/App.jsx
+++ frontend/LmsApp/src/App.jsx
@@ -1,15 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landing.jsx";
 import { Login } from "./pages/login.jsx";
 import { Signup } from "./pages/signup.jsx";
 import Contact from "./pages/contact.jsx";
 import ProtectedRoute from "./components/protectedRoute.jsx";
-// import Admin from "./pages/admin/admin.jsx"
-// import User from "./pages/user/user.jsx";
 import AdminDashboard from "./pages/admin/admin.jsx";
 import UserDashboard from "./pages/user/user.jsx";
+import MobileNotice from "./pages/mobile-notice.jsx";
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const isMobile = window.innerWidth <= 768 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (isMobile && location.pathname !== "/mobile-notice") {
+      navigate("/mobile-notice", { replace: true });
+    }
+  }, [location, navigate]);
+
   return (
     <>
       <Routes>
@@ -17,7 +26,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/contact" element={<Contact />} />
-
         <Route
           path="/admin/dashboard"
           element={
@@ -27,6 +35,7 @@ function App() {
           }
         />
         <Route path="/user/home/" element={<UserDashboard />} />
+        <Route path="/mobile-notice" element={<MobileNotice />} />
       </Routes>
     </>
   );
